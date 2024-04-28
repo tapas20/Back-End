0a432f8e47c1e2cf5816a2033e22de2bae006e52
Tapas Jyoti <tapasjyoti8327@gmail.com>
2024-04-28 19:46:29 +0530

    initial

---
+++ math.js
@@ -0,0 +1,33 @@
+// const sum = (a, b) => a + b;
+// const mul = (a, b) => a * b;
+// const g = 9.8;
+// const PI = 3.14;
+
+// module.exports.sum = (a, b) => a + b;
+// module.exports.mul = (a, b) => a * b;
+// module.exports.g = 9.8;
+// module.exports.PI = 3.14; 
+
+
+// exports.sum = (a, b) => a + b;
+// exports.mul = (a, b) => a * b;
+// exports.g = 9.8;
+// exports.PI = 3.14; 
+
+// export = 5; - (Error)
+
+// module.exports = {
+//     sum: sum, 
+//     mul: mul, 
+//     g: g,
+//     PI: PI,
+// };
+
+// module.exports = obj;
+
+
+//----------Using (import & export)-----------
+export const sum = (a,b) => a+b;
+export const mul = (a,b) => a*b;
+export const g = 9.8;
+export const PI = 3.14;
---
+++ script.js
@@ -0,0 +1,21 @@
+// const math = require("./math");
+
+// console.log(math.sum(2, 2));
+// console.log(math.mul(2, 5));
+// console.log(math.PI);
+
+// const info = require("./Fruits");
+
+// console.log(info[0].name);
+
+
+//----------Import---------
+// import { sum, PI } from "./math.js";
+
+// console.log(sum(1, 2));
+// console.log(PI);
+
+
+import { generate } from "random-words";
+
+console.log(generate());
